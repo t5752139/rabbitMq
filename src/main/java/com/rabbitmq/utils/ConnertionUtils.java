c9f0690a1ff76e99d9f028a98b4ffbbb9f986e04
tyk <www.377134643@qq.com>
2020-06-01 16:41:40 +0800

    1

---
+++ src/main/java/com/rabbitmq/utils/ConnertionUtils.java
@@ -0,0 +1,31 @@
+package com.rabbitmq.utils;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConnertionUtils {
+    /*
+    获取工厂连接
+     */
+   public static Connection getConnection() throws IOException, TimeoutException {
+       //定义一个工厂
+       ConnectionFactory factory = new ConnectionFactory();
+       //设置服务器地址
+       factory.setHost("127.0.0.1");
+       //端口
+       //amqp 5672
+       factory.setPort(5671);
+       //virtual
+       factory.setVirtualHost("/virtual_user");
+       //账号
+       factory.setUsername("user_admin");
+       //密码
+       factory.setPassword("123");
+       //返回连接
+       Connection connection = factory.newConnection();
+       return connection;
+   }
+}
